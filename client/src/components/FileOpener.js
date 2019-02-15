98f7d4db52aacfa776c199db968fa3db05e8c8fd
Nityananda Gohain <nityanandagohain@gmail.com>
2019-02-15 11:39:30 +0530

    file sent to server for further processing

--- client/src/components/FileOpener.js
+++ client/src/components/FileOpener.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import DisplayGene from "./displayGene";
-
+import axios from "axios";
 export class FileOpener extends Component {
   constructor(props) {
     super(props);
@@ -8,34 +8,45 @@ export class FileOpener extends Component {
       details: []
     };
   }
-  handleFileChosen = file => {
+
+  handleFileChosen =async  (file) => {
     var reader = new FileReader();
-    let details = []
+    let contents;
     reader.onload = e => {
-      let contents = reader.result;
-      contents = contents.split(">");
-      if (contents.length > 1) {
-        for (let i = 1; i < contents.length; i++) {
-          if (contents[i].includes("Reverse")) {
-            let content = contents[i].split("Reverse");
-            details.push({"meta": content[0], "gene": content[1]})
-            this.setState({ details: details});
-          } else if (contents[i].includes("Forward")) {
-            let content = contents[i].split("Forward");
-            details.push({"meta": content[0], "gene": content[1]})
-            this.setState({ details: details});
-          } else {
-            alert("Not a fasta file");
-          }
-        }
-        // console.log(this.state.genes);
-        // console.log(this.state.meta);
-      } else {
-        alert("It is not a fasta file");
-      }
-    };
+      contents = reader.result;
+      axios.post("/upload",{
+        "files": contents
+      });
+    }
     reader.readAsText(file);
   };
+  
+  // handleFileChosen = file => {
+  //   var reader = new FileReader();
+  //   let details = []
+  //   reader.onload = e => {
+  //     let contents = reader.result;
+  //     contents = contents.split(">");
+  //     if (contents.length > 1) {
+  //       for (let i = 1; i < contents.length; i++) {
+  //         if (contents[i].includes("Reverse")) {
+  //           let content = contents[i].split("Reverse");
+  //           details.push({"meta": content[0], "gene": content[1]})
+  //           this.setState({ details: details});
+  //         } else if (contents[i].includes("Forward")) {
+  //           let content = contents[i].split("Forward");
+  //           details.push({"meta": content[0], "gene": content[1]})
+  //           this.setState({ details: details});
+  //         } else {
+  //           alert("Not a fasta file");
+  //         }
+  //       }
+  //     } else {
+  //       alert("It is not a fasta file");
+  //     }
+  //   };
+  //   reader.readAsText(file);
+  // };
 
   getGenes = () => {
     return (
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("./config");
+const app = new express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log("Got connection");
+  res.status(200).send({
+    message: "hello"
+  });
+});
+
+app.post("/upload", function(req, res) {
+    console.log("upload");
+    console.log(req.body.files);
+});
+
+app.listen(config.PORT, () => {
+  console.log(`server started at port : ${config.PORT}`);
+});
