c8ecb553c7037c98b00a72949fac5b3b261f9a52
Sourish Sarmah <sourishsarmah@gmail.com>
2019-02-28 01:43:41 +0530

    data stored in mongodb

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const config = require("./config");
 const parseFile = require("./parseFile");
 const app = new express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017"
 
 app.use(bodyParser.json());
 
@@ -18,7 +20,15 @@ app.post("/upload", function(req, res) {
     console.log("upload");
     //console.log(req.body.files);
     let data = parseFile(req.body.files);
-    // console.log(data[1])
+    // console.log(data);
+
+    MongoClient.connect(url, function (err, client) {
+      // if (err) throw err;
+      const db = client.db("gene");
+      var collection = db.collection("gene_data");
+      collection.insertMany(data);
+      client.close();
+  });
 });
 
 app.listen(config.PORT, () => {
